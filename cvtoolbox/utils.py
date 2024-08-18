706b507bbbb737ccefa2fd6166ffca0c41395a65
Ardalan Daneshvar Tarigh <ardalan.dsht@gmail.com>
2024-08-18 21:16:29 +0330

    add load_image_filenames function to utils module

---
+++ cvtoolbox/utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from PIL import Image
+
+valid_file_formats = ["jpeg", "JPEG", "jpg", "JPG", "png", "PNG"]
+
+
+def load_image_filenames(images_root_path: Path) -> list:
+    all_filenames_in_directory = [name for name in images_root_path.iterdir()]
+    image_filenames_in_directory = []
+    for name in all_filenames_in_directory:
+        if name.name.split(".")[-1] in valid_file_formats:
+            image_filenames_in_directory.append(name)
+    return image_filenames_in_directory
