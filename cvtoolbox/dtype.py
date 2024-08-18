fe26b2ec783b56b9e889dd610f82f347a0559c4f
Ardalan Daneshvar Tarigh <ardalan.dsht@gmail.com>
2024-08-18 21:19:23 +0330

    format dtype module with black

--- cvtoolbox/dtype.py
+++ cvtoolbox/dtype.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class CocoBoundingBox:
     """
     These are absolute pixel values for boudingbox.
     """
+
     x_min: int
     y_min: int
     height: int
