2f35a8a8d6d8806bbe65e4aad4787d00a31cbcf9
Ardalan Daneshvar Tarigh <ardalan.dsht@gmail.com>
2024-08-18 21:17:18 +0330

    add load_image_file function to utils module

--- cvtoolbox/utils.py
+++ cvtoolbox/utils.py
@@ -12,3 +12,8 @@ def load_image_filenames(images_root_path: Path) -> list:
         if name.name.split(".")[-1] in valid_file_formats:
             image_filenames_in_directory.append(name)
     return image_filenames_in_directory
+
+
+def load_image_files(image_filenames):
+    loaded_images = [Image.open(image_path) for image_path in image_filenames]
+    return loaded_images
